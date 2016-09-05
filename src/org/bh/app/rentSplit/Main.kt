75688985e46e0790fc60a471ce651e1d1fdb1af8
Ben Leggiero <BenLeggiero@Gmail.com>
2016-09-04 22:41:56 -0400

    Created Kotlin.JS IDEA project

---
+++ src/org/bh/app/rentSplit/Main.kt
@@ -0,0 +1,15 @@
+package org.bh.app.rentSplit
+
+/**
+ * Copyright BHStudios ©2016 BH-1-PS. Made for Rent-Splitting Calculator.
+ *
+ * @author ben_s
+ * @since 004 2016-09-04
+ */
+public class Main {
+    companion object {
+        public fun main(args: Array<String>) {
+            println("Hello, Rent!")
+        }
+    }
+}
